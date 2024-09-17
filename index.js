61b0164c812bac91570eb1b6938adc0431892957
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-17 13:18:32 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  console.log(saturdayFun())
+  console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork())
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(activity = "*") {
+    return function(adjective = "special") {
+        return `You are ${activity}${adjective}${activity}!`;
+      };
+    }
+      
+    
